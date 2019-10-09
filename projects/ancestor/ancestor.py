febf5597aa24ae32b9f47424b117b2f0828eddf2
charlesgodoy <charlesgodoy@live.com>
2019-10-09 07:31:42 -0400

    pushing yesterdays code for ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+def earliest_ancestor(ancestors, starting_node, visited=None):
+    y = [x[1] for x in ancestors]   # used to check if starting node is by finding nothing higher -> -1
+
+    if visited is None:
+        visited = set()
+
+    if starting_node not in y and starting_node not in visited:
+        return -1
+
+    # find a y element that equals starting node, then get its x element, and make that x element new starting node, then repeat
+    for i in ancestors:
+        if i[1] is starting_node:
+            node = i[0]
+            visited.add(node)
+            return earliest_ancestor(ancestors, node, visited)
+    
+    # print(x)
+    # print(y)
+    return starting_node
